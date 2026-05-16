c09bf38fe77881701f279d9ab10f1b5fcfbc21ed
Vodkrad <49844080+Vodkrad@users.noreply.github.com>
2026-05-16 15:14:51 -0700

    Update ChatFlag.lua

--- Flags/ChatFlag.lua
+++ Flags/ChatFlag.lua
@@ -1,4 +1,3 @@
--- https://raw.githubusercontent.com/Vodkrad/Garrysmod/main/Flags/(FLAGNAME)
 --@name Chat Flag Pole
 --@author Jesus Christ
 --@shared
@@ -32,7 +31,7 @@ if SERVER then
             print("Commands:")
             print("!flag canada")
             print("!url https://image.png")
-            print("!git bfg.png")
+            print("!git bfg")
             print("!rotate")
             print("!resetrotate")
             print("!windspeed 5")
@@ -40,22 +39,29 @@ if SERVER then
         end
 
         if string.sub(lower, 1, 6) == "!flag " then
+
             local name = string.sub(lower, 7)
 
             if Flags[name] then
+
                 net.start("change_flag")
                 net.writeString(name)
                 net.writeString(Flags[name])
                 net.send()
+
                 print("Loaded flag for everyone: " .. name)
+
             else
+
                 print("Unknown flag: " .. name)
+
             end
 
             return ""
         end
 
         if string.sub(lower, 1, 5) == "!url " then
+
             local realURL = string.sub(text, 6)
 
             net.start("change_flag")
@@ -64,11 +70,17 @@ if SERVER then
             net.send()
 
             print("Loaded custom URL for everyone")
+
             return ""
         end
 
         if string.sub(lower, 1, 5) == "!git " then
+
             local file = string.sub(text, 6)
+
+            -- AUTO ADD .PNG
+            file = file .. ".png"
+
             local finalURL = GitBase .. file
 
             net.start("change_flag")
@@ -78,26 +90,32 @@ if SERVER then
 
             print("Loaded github flag for everyone")
             print(finalURL)
+
             return ""
         end
 
         if lower == "!rotate" then
+
             net.start("rotate_image")
             net.send()
 
             print("Rotated image 90 degrees right")
+
             return ""
         end
 
         if lower == "!resetrotate" then
+
             net.start("reset_rotate_image")
             net.send()
 
             print("Image rotation reset")
+
             return ""
         end
 
         if string.sub(lower, 1, 11) == "!windspeed " then
+
             local amount = tonumber(string.sub(text, 12)) or 1
 
             net.start("set_windspeed")
@@ -105,6 +123,7 @@ if SERVER then
             net.send()
 
             print("Set windspeed to " .. amount)
+
             return ""
         end
 
@@ -181,8 +200,10 @@ if CLIENT then
     HFlag:setMaterial("brick/brick_model")
 
     for i = 0, HFlag:getBoneCount() - 1 do
+
         HFlag:manipulateBoneScale(i, BoneScale)
         HFlag:manipulateBonePosition(i, Vector(0,0,BoneHeight))
+
     end
 
     local function loadFlagTexture(name, url)
@@ -193,9 +214,12 @@ if CLIENT then
         mat:setTextureURL("$basetexture", url, function(m, u, w, h, l)
 
             if not m then
+
                 print("Failed to load image")
                 print(url)
+
                 return
+
             end
 
             l(0, 0, m:getWidth(), m:getHeight())
@@ -213,12 +237,16 @@ if CLIENT then
     loadFlagTexture(CurrentName, CurrentURL)
 
     net.receive("change_flag", function()
+
         local name = net.readString()
         local url = net.readString()
+
         loadFlagTexture(name, url)
+
     end)
 
     net.receive("rotate_image", function()
+
         ImageRotation = ImageRotation + 90
 
         if ImageRotation >= 360 then
@@ -226,33 +254,43 @@ if CLIENT then
         end
 
         applyImageRotation()
+
         HFlag:setMaterial("!" .. mat:getName())
+
     end)
 
     net.receive("reset_rotate_image", function()
+
         ImageRotation = 0
+
         applyImageRotation()
+
         HFlag:setMaterial("!" .. mat:getName())
+
     end)
 
     net.receive("set_windspeed", function()
+
         WindSpeed = net.readFloat()
+
     end)
 
     hook.add("drawhud", "flag_hud", function()
+
         render.setColor(Color(255,255,255))
 
         render.drawText(50,50,"Commands:",0)
         render.drawText(50,70,"!flags",0)
         render.drawText(50,90,"!flag canada",0)
         render.drawText(50,110,"!url https://image.png",0)
-        render.drawText(50,130,"!git bfg.png",0)
+        render.drawText(50,130,"!git bfg",0)
         render.drawText(50,150,"!rotate",0)
         render.drawText(50,170,"!resetrotate",0)
         render.drawText(50,190,"!windspeed 5",0)
         render.drawText(50,210,"Current: "..CurrentName,0)
         render.drawText(50,230,"Rotation: "..ImageRotation,0)
         render.drawText(50,250,"WindSpeed: "..WindSpeed,0)
+
     end)
 
     timer.create("tick", 0.01, 0, function()
